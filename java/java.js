c73f85a7f723bd1826c5a7c7bda8a05b4d4d6d23
Jonathan Mendes <carisjhow@gmail.com>
2022-12-12 23:31:53 -0300

    object traine

--- java/java.js
+++ java/java.js
@@ -1,6 +1,10 @@
- let amigo = {nome: 'josé', idade: 18, sexo:'M', peso: 75.4, engordar(p){}}
+ let amigo = {nome: 'José', idade: 18, sexo:'M', peso: 75.4, engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+ }}
  
- console.log(amigo.idade)
+
+ console.log(`${amigo.nome} pesa ${amigo.peso} Kg aos ${amigo.idade} anos de idade.`)
 
 
 
